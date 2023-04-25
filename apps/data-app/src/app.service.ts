560b46f6b30ca84eec80fc0cbb69658de0e5f55f
kang,hyungmin <hmkkang0922@daum.net>
2023-04-25 10:21:33 +0900

    add e2e test for data-app

--- apps/data-app/src/app.service.ts
+++ apps/data-app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class DataAppService {
   getHello(): string {
-    return 'Hello Data APP World!';
+    return 'Hello World!';
   }
 }
