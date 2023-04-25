d8ae067cfd29d6ab6dc2893075dc484e28072b2a
kang,hyungmin <hmkkang0922@daum.net>
2023-04-25 10:12:54 +0900

    config tdd setting

--- apps/trade-app/src/app.service.ts
+++ apps/trade-app/src/app.service.ts
@@ -1,18 +1,18 @@
 import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
-import { Cacheable } from 'libs/module/redis/redisCache.const';
+// import { Cacheable } from 'libs/module/redis/redisCache.const';
 
 @Injectable()
 export class AppService {
   constructor() {} // private readonly redisClient: Redis, // @InjectRedis()
 
-  @Cacheable(60)
+  // @Cacheable(60)
   public getHello(): string {
     // this.redisClient.set()
 
-    return 'Hello Trade APP!';
+    return 'Hello World!';
   }
-  @Cacheable(60)
+  // @Cacheable(60)
   public getHello2({ name: string, test: number }, param: string): string {
     // this.redisClient.set()
     return 'Hello Trade APP!';
